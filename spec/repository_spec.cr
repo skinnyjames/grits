6cceffeff877f94ecabb37bc73ebaad1e5862e9b
Sean Gregory <sean.christopher.gregory@gmail.com>
2022-01-31 19:36:44 -0500

    try with system

--- spec/repository_spec.cr
+++ spec/repository_spec.cr
@@ -88,7 +88,7 @@ describe Grits::Repo do
 
       it "via ssh agent" do
         begin
-          system "eval `ssh-agent`"
+          system "eval `ssh-agent -s`"
           `ssh-add #{Fixture.gitea_private_key_path}`
           options = Grits::Cloning::CloneOptions.default
           options.fetch_options.on_credentials_acquire do |credential|
