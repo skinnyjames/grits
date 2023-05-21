413ae23402565648162a81b2f3380fa470e82b88
Sean Gregory <sean.christopher.gregory@gmail.com>
2023-05-20 23:12:34 -0400

    test to narrow segfault in ci

--- spec/worktree_spec.cr
+++ spec/worktree_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-describe Grits::Worktree do
+describe Grits::Worktree, focus: true do
   it "a repo isn't a worktree by default" do
     Fixture.clone_default_http do |repo, path|
       repo.worktree?.should eq(false)
